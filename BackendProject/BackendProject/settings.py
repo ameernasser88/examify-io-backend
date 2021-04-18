dc157320bfbeddf674ff0cac11244dccd18713e6
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2021-04-18 02:57:08 +0200

    middle ware

--- BackendProject/BackendProject/settings.py
+++ BackendProject/BackendProject/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'BackendProject.urls'
