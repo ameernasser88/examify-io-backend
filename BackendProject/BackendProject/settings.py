89bf9bfe50d4cc31c78de4b411ef58b394eff5d3
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2021-05-15 00:07:45 +0200

    Update settings.py

--- BackendProject/BackendProject/settings.py
+++ BackendProject/BackendProject/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "https://examify.vercel.app",
+    "https://localhost:3000",
+    "https://localhost"
 ]
 #CORS_ALLOWED_ORIGINS = []
 # CORS_ORIGIN_ALLOW_ALL = True
