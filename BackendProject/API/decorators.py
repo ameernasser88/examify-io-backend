39ce33bddb89b0689cf3f3f5fbc4c86b07eec460
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2021-05-15 01:48:56 +0200

    decorators updated

--- BackendProject/API/decorators.py
+++ BackendProject/API/decorators.py
@@ -6,8 +6,6 @@ def students_only(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.user_type == "1":
             return view_func(request, *args, **kwargs)
-        elif request.user.is_anonymous:
-            return JsonResponse({'detail': 'Authentication credentials were not provided.'}, status=401)
         else:
             return JsonResponse({'detail':'only students allowed'}, status=403)
     return wrapper_func
@@ -15,9 +13,7 @@ def students_only(view_func):
 
 def examiners_only(view_func):
     def wrapper_func(request, *args, **kwargs):
-        if request.user.is_anonymous:
-            return JsonResponse({'detail': 'Authentication credentials were not provided.'}, status=401)
-        elif request.user.user_type == "2":
+        if request.user.user_type == "2":
             return view_func(request, *args, **kwargs)
         else:
             return JsonResponse({'error':'only examiners allowed'} ,status=403)
